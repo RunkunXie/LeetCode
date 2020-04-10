e162dcab1811cd365a27aa68594d88ee2df2f302
VincentXie <xierunkun@126.com>
2020-04-10 17:02:14 -0400

    challenge week 2 day 3

---
+++ Problems/src/155. Min Stack.py
@@ -0,0 +1,37 @@
+class MinStack:
+    """"""
+
+    """my sol, time 1, space n"""
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append(x)
+        if not self.min_stack:
+            self.min_stack.append(x)
+        else:
+            self.min_stack.append(min(x, self.min_stack[-1]))
+
+    def pop(self) -> None:
+        if self.stack and self.min_stack:
+            self.min_stack.pop()
+            return self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.min_stack:
+            return self.min_stack[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
