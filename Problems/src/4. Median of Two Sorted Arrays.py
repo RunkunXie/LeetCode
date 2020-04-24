5bc4deb14cf6af5397d9d6f2972331b7494bc65a
VincentXie <xierunkun@126.com>
2020-04-23 22:19:18 -0400

    new probs

--- Problems/src/4. Median of Two Sorted Arrays.py
+++ Problems/src/4. Median of Two Sorted Arrays.py
@@ -1,4 +1,60 @@
 class Solution:
+    """my sol, time m + n"""
+    # def findMedianSortedArrays(self, nums1, nums2) -> float:
+    #
+    #     m, n = len(nums1), len(nums2)
+    #
+    #     # base cases
+    #     if m == 0 and n != 0:
+    #         if n % 2 == 1:
+    #             return nums2[n // 2]
+    #         else:
+    #             return (nums2[n // 2 - 1] + nums2[n // 2]) / 2
+    #     elif n == 0 and m != 0:
+    #         if m % 2 == 1:
+    #             return nums1[m // 2]
+    #         else:
+    #             return (nums1[m // 2 - 1] + nums1[m // 2]) / 2
+    #     elif m == 0 and n == 0:
+    #         return []
+    #
+    #     i = 0
+    #     j = 0
+    #     result = []
+    #     midian = (m + n) / 2
+    #
+    #     # loop
+    #     while len(result) <= midian:
+    #
+    #         result.append(min(nums1[i], nums2[j]))
+    #
+    #         if nums1[i] < nums2[j] and i < m - 1:
+    #             i += 1
+    #         elif nums1[i] >= nums2[j] and j < n - 1:
+    #             j += 1
+    #         else:
+    #             if nums1[i] < nums2[j]:
+    #                 last_append_i = 1
+    #                 last_append_j = 0
+    #             else:
+    #                 last_append_i = 0
+    #                 last_append_j = 1
+    #             break
+    #
+    #     while len(result) <= midian and last_append_i:
+    #         result.append(nums2[j])
+    #         j += 1
+    #
+    #     while len(result) <= midian and last_append_j:
+    #         result.append(nums1[i])
+    #         i += 1
+    #
+    #     if (m + n) % 2 == 1:
+    #         return result[-1]
+    #     else:
+    #         return sum(result[-2:]) / 2
+
+    """my sol under hint, time log(n+m)"""
     def findMedianSortedArrays(self, nums1, nums2) -> float:
 
         m, n = len(nums1), len(nums2)
@@ -17,41 +73,52 @@ class Solution:
         elif m == 0 and n == 0:
             return []
 
-        i = 0
-        j = 0
-        result = []
-        midian = (m + n) / 2
+        if m < n:
+            nums1, nums2, m, n = nums2, nums1, n, m
 
-        # loop
-        while len(result) <= midian:
+        total = (m + n)
+        half = int((total + 1) / 2)
 
-            result.append(min(nums1[i], nums2[j]))
+        l, r = 0, n
+        mid2 = int((r + l + 1) / 2)
+        mid1 = half - mid2
 
-            if nums1[i] < nums2[j] and i < m - 1:
-                i += 1
-            elif nums1[i] >= nums2[j] and j < n - 1:
-                j += 1
+        while True:
+            if mid2 < n and nums1[mid1 - 1] > nums2[mid2]:
+                l = mid2
+                mid2 = int((r + l + 1) / 2)
+                mid1 = half - mid2
+            elif mid2 > 0 and nums2[mid2 - 1] > nums1[mid1]:
+                r = mid2
+                mid2 = int((r + l) / 2)
+                mid1 = half - mid2
             else:
-                if nums1[i] < nums2[j]:
-                    last_append_i = 1
-                    last_append_j = 0
-                else:
-                    last_append_i = 0
-                    last_append_j = 1
                 break
 
-        while len(result) <= midian and last_append_i:
-            result.append(nums2[j])
-            j += 1
-
-        while len(result) <= midian and last_append_j:
-            result.append(nums1[i])
-            i += 1
-
-        if (m + n) % 2 == 1:
-            return result[-1]
+        if int(total % 2) == 1:
+            if mid2 == 0:
+                return nums1[mid1 - 1]
+            elif mid1 == 0:
+                return nums2[mid2 - 1]
+            else:
+                return max(nums1[mid1 - 1], nums2[mid2 - 1])
         else:
-            return sum(result[-2:]) / 2
+            if mid2 == 0:
+                if mid1 == m:
+                    return (nums1[mid1 - 1] + nums2[mid2]) / 2
+                return (nums1[mid1 - 1] + min(nums1[mid1], nums2[mid2])) / 2
+            elif mid1 == 0:
+                if mid2 == n:
+                    return (nums2[mid2 - 1] + nums1[mid1]) / 2
+                return (nums2[mid2 - 1] + min(nums2[mid2], nums1[mid1])) / 2
+            elif mid1 == m:
+                return (max(nums1[mid1 - 1], nums2[mid2 - 1]) + nums2[mid2]) / 2
+            elif mid2 == n:
+                return (max(nums1[mid1 - 1], nums2[mid2 - 1]) + nums1[mid1]) / 2
+            else:
+                return (max(nums1[mid1 - 1], nums2[mid2 - 1]) +
+                        min(nums1[mid1], nums2[mid2])) / 2
+
 
 s = Solution()
 print("result:")
---
+++ Problems/src/494. Target Sum.py
@@ -0,0 +1,57 @@
+class Solution:
+    """my sol, bottom up dp"""
+    #     def findTargetSumWays(self, nums: List[int], S: int) -> int:
+
+    #         if abs(S) > 1000:
+    #             return 0
+
+    #         n = len(nums)
+
+    #         dp = [[0] * (n + 1) for _ in range(2001)]
+    #         dp[1000][0] = 1
+
+    #         for j in range(1, n + 1):
+    #             for i in range(2001):
+    #                 if i-nums[j-1] >= 0 and dp[i-nums[j-1]][j-1] > 0:
+    #                     dp[i][j] += dp[i-nums[j-1]][j-1]
+    #                 if i+nums[j-1] <= 2000 and dp[i+nums[j-1]][j-1] > 0:
+    #                     dp[i][j] += dp[i+nums[j-1]][j-1]
+
+    #         return dp[1000+S][n]
+
+    """my sol, top-down dp"""
+
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+
+        # corner case
+        if abs(S) > 1000:
+            return 0
+
+        n = len(nums)
+        memo = {(1000, 0): 1}
+
+        # top-down dp
+        def dp(i, j):
+
+            # memorized case
+            if (i, j) in memo:
+                return memo[i, j]
+
+            # corner case
+            if i < 0 or i > 2000:
+                return 0
+
+            # base case
+            if j == 0:
+                ans = 0
+                if i == 1000:
+                    ans = 1
+
+            # dp case
+            elif j > 0:
+                ans = dp(i - nums[j - 1], j - 1) + dp(i + nums[j - 1], j - 1)
+
+            memo[i, j] = ans
+            return ans
+
+        return dp(1000 + S, n)
