0a753bef98a862fe46b789d11312d6b64390bdce
VincentXie <xierunkun@126.com>
2020-04-25 22:25:43 -0400

    redo q55

--- Problems/src/55. Jump Game.py
+++ Problems/src/55. Jump Game.py
@@ -2,37 +2,27 @@ from typing import List
 
 
 class Solution:
-    """"""
+    """backward greedy"""
+    #     def canJump(self, nums: List[int]) -> bool:
 
-    """my solution, time n, space 1"""
-    # def canJump(self, nums: List[int]) -> bool:
-    #     ans = True
-    #     for num in reversed(nums[:-1]):
-    #         if num == 0 and ans:
-    #             ans = False
-    #             count = 1
-    #
-    #         elif nums == 0 and not ans:
-    #             count += 1
-    #
-    #         elif not ans:
-    #             if num > count:
-    #                 ans = True
-    #             else:
-    #                 count += 1
-    #
-    #     return ans
+    #         n = len(nums)
+    #         last_good = n - 1
+    #         for i in range(n-2, -1, -1):
+    #             if nums[i] >= last_good - i:
+    #                 last_good = i
 
-    """answer, similar to my solution"""
+    #         return last_good == 0
+
+    """forward greedy"""
     def canJump(self, nums: List[int]) -> bool:
 
-        n = len(nums)
-        last_good = n - 1
-        for i in range(n - 2, -1, -1):
-            if nums[i] >= last_good - i:
-                last_good = i
+        max_reach = nums[0]
+        for i, num in enumerate(nums):
+            if max_reach < i:
+                return False
+            max_reach = max(max_reach, i + num)
 
-        return last_good == 0
+        return True
 
 
 print(Solution().canJump([2, 3, 1, 1, 4]), True)
