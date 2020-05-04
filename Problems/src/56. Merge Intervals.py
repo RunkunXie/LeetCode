1e96aecee7e501b4c7d45bba16f1e5ce423ad66c
VincentXie <xierunkun@126.com>
2020-05-04 15:53:44 -0400

    new questions

---
+++ Problems/src/56. Merge Intervals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals = sorted(intervals)
+
+        ans = []
+
+        for i in intervals:
+
+            if not ans:
+                ans.append(i)
+                continue
+
+            if i[0] <= ans[-1][1]:
+                last = ans.pop()
+                ans.append([last[0], max(last[1], i[1])])
+            else:
+                ans.append(i)
+
+        return ans
