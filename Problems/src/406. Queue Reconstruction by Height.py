e8ed015fc40e6520575baf4b7ca61ea134bfaa26
VincentXie <xierunkun@126.com>
2020-05-04 17:57:34 -0400

    new questions

---
+++ Problems/src/406. Queue Reconstruction by Height.py
@@ -0,0 +1,11 @@
+class Solution:
+    """ans, greedy, time """
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people = sorted(people, key=lambda x: (-x[0], x[1]))
+        output = []
+
+        for p in people:
+            output.insert(p[1], p)
+
+        return output
+
