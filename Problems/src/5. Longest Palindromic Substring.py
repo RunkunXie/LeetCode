cfbad6937bd6245d52b5c4519ba269cf5016252a
VincentXie <xierunkun@126.com>
2020-05-04 22:14:37 -0400

    new questions

--- Problems/src/5. Longest Palindromic Substring.py
+++ Problems/src/5. Longest Palindromic Substring.py
@@ -1,39 +1,73 @@
 class Solution:
+    """my sol under hint, time n^2"""
+    # def longestPalindrome(self, s: str) -> str:
+    #
+    #     if not s:
+    #         return ''
+    #
+    #     s = '.'.join(s)
+    #     max_len = 1
+    #     max_p = s[0]
+    #
+    #     for c in range(len(s)):
+    #
+    #         cur_p = ''
+    #         cur_len = 0
+    #
+    #         if s[c] is not '.':
+    #             cur_p = s[c]
+    #             cur_len = 1
+    #
+    #         l = c - 1
+    #         r = c + 1
+    #         while l >= 0 and r < len(s):
+    #             if s[l] != s[r]:
+    #                 break
+    #             elif s[l] == s[r] and s[l] is not '.':
+    #                 cur_p = s[l] + cur_p + s[r]
+    #                 cur_len += 2
+    #             l -= 1
+    #             r += 1
+    #
+    #         if cur_len > max_len:
+    #             max_len = cur_len
+    #             max_p = cur_p
+    #
+    #     return max_p
+
+    """my dp sol, time n^2"""
     def longestPalindrome(self, s: str) -> str:
 
         if not s:
             return ''
 
-        s = '.'.join(s)
+        n = len(s)
+        if n == 1:
+            return s
+
         max_len = 1
-        max_p = s[0]
+        ans = s[0]
 
-        for c in range(len(s)):
+        dp = [[0] * n for _ in range(n)]
+        for j in range(n):
+            for i in range(j, -1, -1):
 
-            cur_p = ''
-            cur_len = 0
+                if i == j:
+                    dp[i][j] = 1
 
-            if s[c] is not '.':
-                cur_p = s[c]
-                cur_len = 1
+                elif i == j - 1 and s[i] == s[j]:
+                    dp[i][j] = 1
 
-            l = c - 1
-            r = c + 1
-            while l >= 0 and r < len(s):
-                if s[l] != s[r]:
-                    break
-                elif s[l] == s[r] and s[l] is not '.':
-                    cur_p = s[l] + cur_p + s[r]
-                    cur_len += 2
-                l -= 1
-                r += 1
+                elif dp[i + 1][j - 1] and s[i] == s[j]:
+                    dp[i][j] = 1
 
-            if cur_len > max_len:
-                max_len = cur_len
-                max_p = cur_p
+                if dp[i][j]:
+                    cur_len = j - i + 1
+                    if cur_len > max_len:
+                        max_len = cur_len
+                        ans = s[i:j + 1]
 
-        return max_p
-
+        return ans
 
 print(Solution().longestPalindrome('abdde'))
 print(Solution().longestPalindrome('babad'))
