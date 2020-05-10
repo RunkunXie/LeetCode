15e0514f099fb5f9733eb99124706bfecf682dad
VincentXie <xierunkun@126.com>
2020-05-10 10:46:04 -0400

    lc day10

--- Problems/src/997. Find the Town Judge.py
+++ Problems/src/997. Find the Town Judge.py
@@ -1,4 +1,5 @@
 class Solution:
+    """my sol, 1st attempt, time E + V"""
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
 
         if N == 1:
@@ -19,4 +20,36 @@ class Solution:
 
         return judge[0] if len(judge) == 1 else -1
 
+    """answer 1 - graph approach"""
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        if len(trust) < N - 1:
+            return -1
 
+        indegree = [0] * (N + 1)
+        outdegree = [0] * (N + 1)
+
+        for a, b in trust:
+            outdegree[a] += 1
+            indegree[b] += 1
+
+        for i in range(1, N + 1):
+            if indegree[i] == N - 1 and outdegree[i] == 0:
+                return i
+        return -1
+
+    """ans 2, best approach"""
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+
+        if len(trust) < N - 1:
+            return -1
+
+        trust_scores = [0] * (N + 1)
+
+        for a, b in trust:
+            trust_scores[a] -= 1
+            trust_scores[b] += 1
+
+        for i, score in enumerate(trust_scores[1:], 1):
+            if score == N - 1:
+                return i
+        return -1
