4ec6c9e004e67f9dc44979c381e9f5b12ca0221d
VincentXie <xierunkun@126.com>
2020-05-17 12:28:43 -0400

    Weekly Contest 189 - Q3

--- Problems/src/1452. People Whose List of Favorite Companies Is Not a Subset of Another List.py
+++ Problems/src/1452. People Whose List of Favorite Companies Is Not a Subset of Another List.py
@@ -1,1 +1,20 @@
 """Weekly Contest 189 - Q3"""
+
+
+class Solution:
+    """my set col, time n^2, beat 100, 100"""
+    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
+
+        fc_set = [set(com) for com in favoriteCompanies]
+        ans = []
+
+        for i in range(len(fc_set)):
+            distinct = True
+            for j in range(len(fc_set)):
+                if i is not j and fc_set[i].issubset(fc_set[j]):
+                    distinct = False
+                    break
+            if distinct:
+                ans.append(i)
+
+        return ans
