c99b238ce132aa90278acd3314ebd924e2d8ee68
VincentXie <xierunkun@126.com>
2020-06-07 11:28:08 -0400

    weekly contest 192 Q1

--- Problems/src/1470. Shuffle the Array.py
+++ Problems/src/1470. Shuffle the Array.py
@@ -1,4 +1,5 @@
 class Solution:
+    """my sol time n"""
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         ans = []
 
@@ -7,3 +8,10 @@ class Solution:
             ans.append(nums[i + n])
 
         return ans
+
+    """online zip sol"""
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i, j in zip(nums[0:len(nums) // 2], nums[len(nums) // 2:]):
+            res += [i, j]
+        return res
