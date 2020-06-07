d7157379e874c8234291b12ad1dc97c506faafe8
VincentXie <xierunkun@126.com>
2020-06-07 12:12:12 -0400

    new questions

---
+++ Problems/src/256. Paint House.py
@@ -0,0 +1,15 @@
+class Solution:
+    """my dp sol (state machine?), time n"""
+    def minCost(self, costs: List[List[int]]) -> int:
+
+        if not costs:
+            return 0
+
+        cur_costs = costs[0]
+
+        for c in costs[1:]:
+            cur_costs = [c[0] + min(cur_costs[1], cur_costs[2]),
+                         c[1] + min(cur_costs[0], cur_costs[2]),
+                         c[2] + min(cur_costs[0], cur_costs[1])]
+
+        return min(cur_costs)
