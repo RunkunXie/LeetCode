2a9e7a531ad95d391705b595caf8d2a98ca416d5
VincentXie <xierunkun@126.com>
2020-06-15 18:24:30 -0400

    lc day13,14,15

---
+++ Problems/src/787. Cheapest Flights Within K Stops.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+
+        graph = defaultdict(list)
+        for f in flights:
+            graph[f[0]].append(f[1:])
+        visited = [False] * n
+        self.ans = float("inf")
+
+        def dfs(node=src, dst=dst, K=K, price=0):
+
+            if node == dst:
+                self.ans = min(self.ans, price)
+                return
+
+            if K == -1 or price > self.ans:
+                return
+
+            for adj, w in graph[node]:
+                if not visited[adj]:
+                    visited[adj] = True
+                    dfs(adj, dst, K - 1, price + w)
+                    visited[adj] = False
+
+        dfs()
+        return self.ans if self.ans != float("inf") else -1
