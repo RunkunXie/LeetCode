6e9a5b2d7abbde827d1c80c16963041b236686b7
VincentXie <xierunkun@126.com>
2020-06-16 17:28:50 -0400

    qishi course week3.linkedlist - eg8

--- Problems/src/11. Container With Most Water.py
+++ Problems/src/11. Container With Most Water.py
@@ -1,5 +1,22 @@
 class Solution:
 
+    """my sol, 2nd attempt"""
+    def maxArea(self, height: List[int]) -> int:
+
+        start, end, res = 0, len(height) - 1, 0
+
+        while start < end:
+
+            h = min(height[start], height[end])
+            res = max(res, h * (end - start))
+
+            if height[start] <= height[end]:
+                start += 1
+            else:
+                end -= 1
+
+        return res
+
     def maxArea(self, height: List[int]) -> int:
 
         i, j = 0, len(height) - 1
